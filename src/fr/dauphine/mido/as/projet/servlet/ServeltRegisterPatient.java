2b98334936579d09617914749dfb6051715b5ec0
alexSchwitthal <schwitthal.alexandre@gmail.com>
2021-01-20 03:16:47 +0100

    début servletRegisterPatient (not working)

--- src/fr/dauphine/mido/as/projet/servlet/ServeltRegisterPatient.java
+++ src/fr/dauphine/mido/as/projet/servlet/ServeltRegisterPatient.java
@@ -1,11 +1,20 @@
 package fr.dauphine.mido.as.projet.servlet;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.dauphine.mido.as.projet.beans.Adresse;
+import fr.dauphine.mido.as.projet.beans.Patient;
+import fr.dauphine.mido.as.projet.beans.Personne;
 
 /**
  * Servlet implementation class ServeltRegisterPatient
@@ -34,9 +43,45 @@ public class ServeltRegisterPatient extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("test");
-		// TODO Auto-generated method stub
-		//doGet(request, response);
+		try {
+	        response.setContentType("text/html");
+	        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+	        // NOT WORKING
+	        EntityManagerFactory emf = Persistence.createEntityManagerFactory("projet-SAJ");
+	        //EntityManager em = emf.createEntityManager();
+	            
+	        Personne personne = new Personne();
+	        Patient patient = new Patient();
+	        Adresse adresse = new Adresse();
+	        response.setContentType("text/html");
+
+	        personne.setNom(request.getParameter("nom"));
+	        personne.setPrenom(request.getParameter("prenom"));
+	        personne.setDateNaissance(sdf.parse(request.getParameter("dateNaissance")));
+
+	        adresse.setAdresseComplete(request.getParameter("adresse"));
+	        adresse.setCodePostal(request.getParameter("cp"));
+	        adresse.setPays(request.getParameter("pays"));
+	        
+	        patient.setEmail(request.getParameter("email"));
+	        patient.setTelephone(request.getParameter("tel"));
+	        patient.setMotDePasse(request.getParameter("mdp"));
+	        
+	        patient.setPersonne(personne);
+	        personne.setAdresse(adresse);
+
+	     //   em.getTransaction().begin();
+	    //    em.persist(adresse);
+	    //    em.persist(personne);
+	     //   em.persist(patient);
+	    //   em.getTransaction().commit();
+	      
+	       //em.close();
+	        emf.close();
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        };
 	}
 
 }
